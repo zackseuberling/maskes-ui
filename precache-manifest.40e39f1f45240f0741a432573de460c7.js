e5f8f1b65805564aecef56fc4b585db6db82dc10
Kha Nguyen <ykha@uw.edu>
2020-07-02 10:48:13 -0700

    Updates

---
+++ precache-manifest.40e39f1f45240f0741a432573de460c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0071cfb103738e7f915e2ef4ae4d0685",
+    "url": "/maskes-ui/index.html"
+  },
+  {
+    "revision": "fd94202bdaa230446b6d",
+    "url": "/maskes-ui/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e8aa8938cb6813dea895",
+    "url": "/maskes-ui/static/css/main.26fc38bc.chunk.css"
+  },
+  {
+    "revision": "fd94202bdaa230446b6d",
+    "url": "/maskes-ui/static/js/2.60a1dbd8.chunk.js"
+  },
+  {
+    "revision": "ccb07d3f36c83d118d798038f894bf02",
+    "url": "/maskes-ui/static/js/2.60a1dbd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8aa8938cb6813dea895",
+    "url": "/maskes-ui/static/js/main.e8258a4d.chunk.js"
+  },
+  {
+    "revision": "ab7c1d1b1e03f487cddb",
+    "url": "/maskes-ui/static/js/runtime-main.88357c5f.js"
+  }
+]);

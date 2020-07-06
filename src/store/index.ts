7fcaf3517107343692729307f456bbd11cff0513
Duc Vo <pianote@gmail.com>
2020-07-06 14:26:16 -0700

    add thunk middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,11 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 // import globalModals from '../components/global-modals/login/login-modal.reducers';
 import auth from '../components/global-modals/auth/store/reducer/reducer';
+import thunk from 'redux-thunk';
 
 
 const appReducers = combineReducers({
   auth,
 });
 
-const store = createStore(appReducers, applyMiddleware(logger));
+const store = createStore(appReducers, applyMiddleware(logger, thunk));
 export default store;

f61b4c421c29252c9d1a907faef0933d53be86b3
Duc Vo <pianote@gmail.com>
2020-07-08 20:58:35 -0700

    add ProtectedRoute for authenticated users

--- src/components/root/AppRoot.tsx
+++ src/components/root/AppRoot.tsx
@@ -7,11 +7,22 @@ import Navbar from '../navbar/Navbar';
 import Breadcrumbs from '../breadcrumbs/Breadcrumbs';
 import GlobalModals from '../global-modals/GlobalModals';
 import Logout from '../global-modals/auth/Logout';
+
+import Home from '../home/Home';
+import GetHelp from '../get-help/GetHelp';
+import Volunteer from '../volunteer/Volunteer';
+
+import ManageRequests from '../manage-requests/manage-request-page/ManageRequests';
+import CreateRequestPage from '../manage-requests/create-request-page/CreateRequestPage';
+import ViewRequestDetail from '../manage-requests/manage-request-page/view-request-detail/ViewRequestDetail';
+
+
+import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 import { withRouter } from 'react-router';
 import * as navManager from '../navbar/nav-manager';
 import { authCheckLoginState } from '../global-modals/auth/store/actions/actions';
 
-const AppRoot = ({ component, authCheckLoginState }) => {
+const AppRoot = ({ component, authCheckLoginState, hasLogin }) => {
   useEffect(() => {
     authCheckLoginState()
   })
@@ -19,9 +30,16 @@ const AppRoot = ({ component, authCheckLoginState }) => {
     <div>
       <GlobalModals />
       <Navbar />
-      {/* TODO: hide breadcrumbs for protected route */}
-      <Breadcrumbs />
-      <Route path="/:navId?/:subNavId?" component={component}></Route>
+      {hasLogin ? <Breadcrumbs /> : null}
+      <Route exact path='/' component={Home} />
+      <Route exact path='/get-help' component={GetHelp} />
+      <Route exact path='/volunteer' component={Volunteer} />
+      <ProtectedRoute exact path='/my-requests' component={ManageRequests} />
+      <ProtectedRoute exact path='/my-requests/create-request' component={CreateRequestPage} />
+      <ProtectedRoute path='/my-requests/:requestId' component={ViewRequestDetail} />
+
+      {/* <Route path="/:navId?/:subNavId?" component={component} /> */}
+
       <Route exact path="/logout" component={Logout} />
     </div>
   )
@@ -34,6 +52,7 @@ const mapStateToProps = (state, props) => {
 
   return {
     component: navManager.getDisplayComponentForNav(state, params),
+    hasLogin: state.auth.access !== null
   };
 };
 

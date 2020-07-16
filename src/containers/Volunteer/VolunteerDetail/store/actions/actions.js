89700eb95b1b7c6fb9fb42754689cf57eae4714b
Duc Vo <pianote@gmail.com>
2020-07-16 16:23:55 -0700

    Volunteer redux store initial commit

---
+++ src/containers/Volunteer/VolunteerDetail/store/actions/actions.js
@@ -0,0 +1,6 @@
+export const fetchSignedUpRequestDetail = (props) => (
+    <div>
+        <p>dispatch some action here</p>
+        <p>this will be a detail signed up request</p>
+    </div>
+);
---
+++ src/containers/Volunteer/VolunteerList/store/actions/actions.js
@@ -0,0 +1,7 @@
+export const fetchOpenRequests = (props) => (
+    <div>
+        <p>dispatch some actions here</p>
+        <p>this will be a list of "In Process" requests</p>
+        <p>volunteer can filter their signed up requests</p>
+    </div>
+);

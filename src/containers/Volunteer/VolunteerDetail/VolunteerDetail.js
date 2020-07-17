1daae157fb9ad548097da771ea288cb9ca0c2e0a
Duc Vo <pianote@gmail.com>
2020-07-17 15:23:00 -0700

    build temp ui and funtionalities for volunteer

--- src/containers/Volunteer/VolunteerDetail/VolunteerDetail.js
+++ src/containers/Volunteer/VolunteerDetail/VolunteerDetail.js
@@ -1,7 +1,65 @@
 import React from 'react';
+import Aux from '../../../hoc/Aux/Aux';
+import { Spinner, Button, Container, Table } from 'react-bootstrap';
+import Volunteer from '../Volunteer';
+import { connect } from 'react-redux';
 
-const VolunteerDetail = (props) => (
-    <div>[Volunteer Detail]: Show signed up request detail<br /><button className='btn btn-primary'>Sign up</button></div>
-);
+const VolunteerDetail = (props) => {
 
-export default VolunteerDetail;
+    const data = [{
+        id: 1,
+        created_date: "2020-06-29T20:42:40.642816Z",
+        location: "Des Moines",
+        household_number: 3,
+        food_restrictions: "None",
+        items_list: "Eggs, bread, chicken drumsticks, pork, brown rice, whole wheat bread, cooking oil, brown sugar, toliet paper, paper towel, brocolli, sweet peas, eggplants, green beans, corn, watermelon, strawberries, cherries",
+        urgency: "In the next 72 hours",
+    }]
+
+    let display = data.map((request, index) => (
+        <Aux key={index}>
+
+            <Table bordered striped hover size="sm" responsive='sm'>
+                <tbody >
+                    <tr><td>Created Date</td><td>{new Date(request.created_date).toLocaleDateString()}</td></tr>
+                    <tr><td>locations</td><td>{request.location}</td></tr>
+                    <tr><td>List of Items</td><td style={{ width: '80%' }}>{request.items_list}</td></tr>
+                    <tr><td>Food Restrictions</td><td>{request.food_restrictions}</td></tr>
+                    <tr><td>Household Size</td><td>{request.household_number}</td></tr>
+                    <tr><td>Urgency</td><td>{request.urgency}</td></tr>
+                </tbody>
+            </Table>
+
+            <Button size='lg'
+                className='mt-1 mb-3'
+                onClick={() => alert('Thank you for signing up this request')}
+            >Volunteer</Button>
+        </Aux>
+    ));
+
+    return (
+        <Volunteer name={props.name}>
+            <Container fluid>
+                <h3>Request Detail</h3>
+                {false
+                    ? <Spinner animation="border" style={{ marginLeft: '40%' }} />
+                    : display
+
+                }
+            </Container>
+        </Volunteer>
+    );
+};
+
+const mapStateToProps = (state) => {
+    return {
+        hasLogin: state.auth.access !== null,
+        token: state.auth.access,
+        name: state.auth.name
+        // loading: state.volunteerDetail.loading,
+        // requests: state.volunteerDetail.requests,
+
+    }
+}
+
+export default connect(mapStateToProps, null)(VolunteerDetail);
--- src/containers/Volunteer/VolunteerList/VolunteerList.js
+++ src/containers/Volunteer/VolunteerList/VolunteerList.js
@@ -1,18 +1,76 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Spinner from 'react-bootstrap/Spinner';
 import Button from 'react-bootstrap/Button';
 import { Row, Col, Form } from 'react-bootstrap';
 import { connect } from 'react-redux';
-// import { fetchRequests } from './store/actions/actions';
+import { BsBoxArrowInRight } from 'react-icons/bs';
 import Table from 'react-bootstrap/Table';
 import Container from 'react-bootstrap/Container';
 import Volunteer from '../Volunteer';
 
 const VolunteerList = (props) => {
-    const { name } = props
-    const [isSignedUp1, setIsSignedUp1] = useState(false);
-    const [isSignedUp2, setIsSignedUp2] = useState(false);
-    const [isSignedUp3, setIsSignedUp3] = useState(false);
+    const { name, match } = props
+
+    const [requests, setRequests] = useState([]);
+
+    const toggleChosen = (id) => setRequests(
+        requests.map((request, index) =>
+            (index === id)
+                ? {
+                    ...request,
+                    is_chosen: !request.is_chosen
+                }
+                : request)
+    );
+
+    const data = [
+        {
+            location: 'Kent',
+            household_number: '4',
+            urgency: 'ASAP',
+            created_date: new Date().toLocaleDateString()
+        },
+        {
+            location: 'Renton',
+            household_number: '7',
+            urgency: 'Over the next few days',
+            created_date: new Date().toLocaleDateString()
+        },
+        {
+            location: 'Federal Way',
+            household_number: '3',
+            urgency: 'Useful if available',
+            created_date: new Date().toLocaleDateString()
+        },
+    ];
+
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        setRequests(data.map((request) => ({ ...request, is_chosen: false })));
+        setTimeout(() => {
+            setLoading(false)
+        }, [500])
+        // eslint-disable-next-line
+    }, [])
+
+    let display = requests.map((request, index) => (
+        <tr key={index} style={request.is_chosen ? { backgroundColor: '#E1F6F1' } : null}>
+            <td>{index + 1}</td>
+            <td>{request.location}</td>
+            <td>{request.household_number}</td>
+            <td>{request.urgency}</td>
+            <td>{request.created_date}</td>
+            <td style={{ width: '100px' }}>
+                <Button size='sm' block
+                    disabled={request.is_chosen}
+                    variant={request.is_chosen ? "disable" : "link"}
+                    onClick={() => toggleChosen(index)}
+                >{request.is_chosen ? "Signed Up" : "Avaiable"}</Button>
+            </td>
+            <td style={{ textAlign: 'center' }}><a href={`${match.url}/${index}`}><BsBoxArrowInRight style={{ width: '20px', height: '20px' }} /></a></td>
+        </tr>
+    ));
+
     return (
         <Volunteer name={name}>
             <Container fluid>
@@ -53,8 +111,8 @@ const VolunteerList = (props) => {
                         </Col>
                     </Row>
                 </Form>
-                {false
-                    ? <Spinner animation="grow" />
+                {loading
+                    ? <Spinner animation="border" style={{ marginLeft: '40%' }} />
                     : <Table striped bordered hover size="sm" responsive='sm'>
                         <thead>
                             <tr>
@@ -64,39 +122,11 @@ const VolunteerList = (props) => {
                                 <th>Urgent Needs</th>
                                 <th>Date Created</th>
                                 <th>Status</th>
+                                <th></th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr style={isSignedUp1 ? { backgroundColor: '#E1F6F1' } : null}>
-                                <td>1</td>
-                                <td>Kent</td>
-                                <td>4</td>
-                                <td>ASAP</td>
-                                <td>{new Date().toLocaleDateString()}</td>
-                                <td style={{ width: '100px' }}><Button size='sm' block
-                                    variant={isSignedUp1 ? "success" : "primary"}
-                                    onClick={() => setIsSignedUp1(!isSignedUp1)}>{isSignedUp1 ? "Signed Up" : "Sign Up"}</Button></td>
-                            </tr>
-                            <tr style={isSignedUp2 ? { backgroundColor: '#E1F6F1' } : null}>
-                                <td>2</td>
-                                <td>Renton</td>
-                                <td>7</td>
-                                <td>Over the next few days</td>
-                                <td>{new Date().toLocaleDateString()}</td>
-                                <td><Button size='sm' block
-                                    variant={isSignedUp2 ? "success" : "primary"}
-                                    onClick={() => setIsSignedUp2(!isSignedUp2)}>{isSignedUp2 ? "Signed Up" : "Sign Up"}</Button></td>
-                            </tr>
-                            <tr style={isSignedUp3 ? { backgroundColor: '#E1F6F1' } : null}>
-                                <td>3</td>
-                                <td>Federal Way</td>
-                                <td>3</td>
-                                <td>Useful if available</td>
-                                <td>{new Date().toLocaleDateString()}</td>
-                                <td><Button size='sm' block
-                                    variant={isSignedUp3 ? "success" : "primary"}
-                                    onClick={() => setIsSignedUp3(!isSignedUp3)}>{isSignedUp3 ? "Signed Up" : "Sign Up"}</Button></td>
-                            </tr>
+                            {display}
                         </tbody>
                     </Table>
                 }
@@ -111,8 +141,8 @@ const mapStateToProps = (state) => {
         hasLogin: state.auth.access !== null,
         token: state.auth.access,
         name: state.auth.name
-        // loading: state.requestList.loading,
-        // requests: state.requestList.requests,
+        // loading: state.volunteerList.loading,
+        // requests: state.volunteerList.requests,
 
     }
 }

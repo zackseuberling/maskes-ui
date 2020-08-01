d24653510ada2f3302f2adcdb3d2d27cad6f3030
Duc Vo <pianote@gmail.com>
2020-07-31 20:39:35 -0700

    embed volunteer form

--- src/containers/GetInvolved/GetInvolved.js
+++ src/containers/GetInvolved/GetInvolved.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Container, ResponsiveEmbed } from 'react-bootstrap';
 
 const GetInvolved = (props) => (
-    <div>Get Involved</div>
+    <Container fluid='sm' style={{ height: "8020" }}>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdqoK6a4mmYc2tpVPVTDfq2EDjsSMDct8Am5duoCx44i-fIoQ/viewform?embedded=true"
+            width="100%" height="8020" frameborder="0" marginheight="0" marginwidth="0"
+            title='Offer Support Form' >Loading…</iframe>
+    </Container>
 );
 
 export default GetInvolved;
